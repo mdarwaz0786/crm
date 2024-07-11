1137714225d780032a7515a1eab7c144f7101acd
Md Arwaz <mdarwaznew2017@gmail.com>
2024-07-11 14:42:33 +0530

    change jsw token expire in limit

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (userId, name, email, mobile, password, role) => {
-  return jwt.sign({ userId, name, email, mobile, password, role }, process.env.JWT_SECRET_KEY, { expiresIn: "365d" });
+  return jwt.sign({ userId, name, email, mobile, password, role }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
 };
 
 export default generateToken;
